d50325f6eaa403b0749b5f1a25893c012a2b2679
a456qwerty654 <asay6697@gmail.com>
2021-10-23 12:28:12 +1100

    Volcano map - Added colour-coding to markers based on elevation

--- python_apps/interactive_volcano_map/create_map.py
+++ python_apps/interactive_volcano_map/create_map.py
@@ -17,11 +17,23 @@ volcano_points = folium.FeatureGroup(name='Volcanoes')
 volcano_lat = list(volcano_file['LAT'])
 volcano_lon = list(volcano_file['LON'])
 volcano_name = list(volcano_file['NAME'])
+volcano_elevation = list(volcano_file['ELEV'])
+
+#Function to set colour of volcano based on elevation level
+def setVolcanoMarkerColour(elevation):
+    if elevation >= 3000:
+        return 'red'
+    elif elevation >= 2000:
+        return 'orange'
+    elif elevation >= 1000:
+        return 'green'
+    else:
+        return 'blue'
 
 #add all the volcano markers to the feature group
-for lat, lon, name in zip(volcano_lat, volcano_lon, volcano_name):
+for lat, lon, name, elevation in zip(volcano_lat, volcano_lon, volcano_name, volcano_elevation):
     #iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    volcano_points.add_child(folium.Marker(location=[lat, lon], popup=folium.Popup(name), icon=folium.Icon(color='red')))
+    volcano_points.add_child(folium.Marker(location=[lat, lon], popup=folium.Popup(name), icon=folium.Icon(color=setVolcanoMarkerColour(elevation=elevation))))
 
 #add the feature group with the volcano markers to the map
 map.add_child(volcano_points)
